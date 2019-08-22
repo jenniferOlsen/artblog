347cbe2444bee6a22480ef68061a2cf69db2600c
Jenny Olsen <jenny.a.olsen@gmail.com>
2019-08-22 12:36:48 -0700

    set up eslint

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,16 +4,15 @@ import '../assets/scss/main.scss'
 import Header from './Header'
 
 class Template extends React.Component {
-    render() {
-        const { children } = this.props
-
-        return (
-            <div>
-                <Header />
-                {children}
-            </div>
-        )
-    }
+  render() {
+    const { children } = this.props
+    return (
+      <div>
+        <Header />
+        {children}
+      </div>
+    )
+  }
 }
 
 export default Template
