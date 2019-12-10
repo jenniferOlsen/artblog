35d395bf7bab2edc807831a8d7bdf9d2e886cdf8
Jenny Olsen <jenny.a.olsen@gmail.com>
2019-12-10 15:38:12 -0800

    add contact form

---
+++ src/pages/thanks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <div id="main">
+      <h1>Thank you!</h1>
+      <p>I'll respond to your message as soon as possible.</p>
+    </div>
+  </Layout>
+)
